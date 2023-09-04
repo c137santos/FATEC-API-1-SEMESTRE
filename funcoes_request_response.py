8b3b536dbe30fa4164e361c8879d7e90560e7be9
marilia-borgo <marilia.bm01@gmail.com>
2023-09-04 19:40:49 -0300

    adiciona camanda de pegar as coisas do json

--- funcoes_request_response.py
+++ funcoes_request_response.py
@@ -1,6 +1,15 @@
 from wgsi import HttpResponse
+from regra_de_negocio.service import busca_dados_json
 
 def hola_mundinho(request):
    response  =  HttpResponse({}, "hey mundinho", "", "200 OK")
 
    return response
+
+def busca_arquivos_json(request):
+   dados = busca_dados_json()
+   dados_string = str(dados)
+   response  =  HttpResponse({}, dados_string, "", "200 OK")
+
+   return response
+
--- urls.py
+++ urls.py
@@ -1,7 +1,8 @@
-from funcoes_request_response import hola_mundinho
+from funcoes_request_response import hola_mundinho, busca_arquivos_json
 
 def url_match(url):
     url_dict = {
-        "/" : hola_mundinho
+        "/" : hola_mundinho,
+        "/get_json": busca_arquivos_json,
     }
     return url_dict[url]
