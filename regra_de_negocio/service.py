228d46cf6704886c31d4bcdc2f8365bb3ee7a041
marilia-borgo <marilia.bm01@gmail.com>
2023-09-14 14:47:40 -0300

    retorna os alunos json (só para não quebrar)

--- regra_de_negocio/service.py
+++ regra_de_negocio/service.py
@@ -1,6 +1,6 @@
 import json
 
 def busca_dados_json():
-    with open("dados/bancodedados.json", "r") as f:
+    with open("dados/alunos.json", "r") as f:
         data = json.load(f)
         return data
