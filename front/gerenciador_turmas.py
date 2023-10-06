a8f9adbd0ceff973416394829e0f72d73d1230ce
MateusMSoares <mateus.marques100@hotmail.com>
2023-10-06 19:48:59 -0300

    ajustado logico para relacionar tabela grupos_alunos e alunos

--- front/gerenciador_turmas.py
+++ front/gerenciador_turmas.py
@@ -28,14 +28,14 @@ def busca_alunos():
 # Esta função busca grupos e alunos de uma turma específica
 def busca_info_turmas(id):
     grupos = busca_grupos_turma(id)
-    alunos = busca_grupos_alunos(grupos)
+    alunos_grupo = busca_grupos_alunos(grupos)
+    alunos = busca_info_alunos(alunos_grupo)
     return grupos, alunos
-
 
 # Esta função busca grupos de uma turma específica
 def busca_grupos_turma(id):
-    grupo_data = {}  # Dicionário para armazenar grupos
     grupo_data_raw = busca_grupos()  # Buscar dados brutos dos grupos
+    grupo_data = {}  # Dicionário para armazenar grupos
     for grupo_id, grupo_info in grupo_data_raw.items():
         if grupo_info["turma"] == int(id):  # Verificar se o grupo pertence à turma especificada
             nome_grupo = grupo_info["nome"]  # Obter o nome do grupo
@@ -44,22 +44,22 @@ def busca_grupos_turma(id):
 
 # Esta função busca alunos associados a grupos
 def busca_grupos_alunos(grupos):
-    alunos_data_raw = busca_alunos()  # Buscar dados brutos dos alunos
-    alunos_data = {}  # Dicionário para armazenar alunos
-    for id_aluno, aluno_info in alunos_data_raw.items():
-        if id_aluno in grupos:  # Verificar se o aluno está associado a um dos grupos
-            nome_aluno = aluno_info["nome"]  # Obter o nome do aluno
-            alunos_data[id_aluno] = nome_aluno  # Adicionar o aluno ao dicionário de alunos
-    return alunos_data  # Retornar os alunos associados aos grupos
+    grupos_alunos_data_raw = busca_grupo_alunos()  # Buscar dados brutos de alunos e seus respectivos grupos
+    alunos = []
+    grupos =  grupos.keys()
+    # Iterar sobre os grupos da turma especificada
+    for grupo_id in grupos:
+        for aluno_id, aluno_info in grupos_alunos_data_raw.items():
+            if aluno_info["grupo"] == int(grupo_id):  # Comparar a chave do grupo com o valor de "grupo" em aluno_info
+                alunos.append(aluno_id)
+    return alunos
 
 # Esta função busca os nomes dos alunos
-def busca_info_alunos(alunos):
+def busca_info_alunos(alunos_grupo):
     alunos_data_raw = busca_alunos()  # Buscar dados brutos dos alunos
-    alunos_data = {}  # Dicionário para armazenar nomes de alunos
-    for id_aluno, nome_aluno in alunos.items():
-        alunos_data[id_aluno] = nome_aluno  # Adicionar o nome do aluno ao dicionário de nomes de alunos
-    return alunos_data  # Retornar os nomes dos alunos
-
-
-
-
+    alunos_data = {}  # Dicionário para armazenar informações dos alunos
+    for aluno_id, aluno_info in alunos_data_raw.items():
+        if aluno_id in alunos_grupo:
+            nome_aluno = aluno_info["nome"]  # Obter o nome do aluno
+            alunos_data[aluno_id] = nome_aluno  # Adicionar o nome do aluno ao dicionário de nomes de alunos
+    return alunos_data  # Retornar as informações dos alunos
