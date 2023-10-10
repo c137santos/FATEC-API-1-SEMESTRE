cbfd4ca90c2f58ec31a69b36d182193a27185e97
Sara Nahra <sara.nahra@fatec.sp.gov.br>
2023-10-10 20:49:55 -0300

    adiciona a edição de turmas

--- regra_de_negocio/gerenciador_turmas.py
+++ regra_de_negocio/gerenciador_turmas.py
@@ -1,62 +1,40 @@
 import json
 
-# Retorna:
-#    um dicionário onde cada turma é um par chave-valor
-def obter_todas_turmas():
-    with open("dados/turmas.json", 'r', encoding="utf-8") as arquivo:
-        dados = arquivo.read()
-        turmas = json.loads(dados)
-        return turmas
-
-# Retorna
-#    o último id disponível.
-def obter_ultimo_id():
-    return sorted(obter_todas_turmas().keys()).pop()
-
-# Retorna:
-#    o próximo id disponível para uma nova turma.
-def obter_novo_id():
-    ultimo_id = obter_ultimo_id()
-    if ultimo_id:
-        return str(int(ultimo_id) + 1)
-    else:
-        return str(1)
-
-# Parâmetro: um dicionário onde cada turma é um par chave-valor
-def salvar_turmas(turmas):
-    dados = json.dumps(turmas, indent=4)
-    with open("dados/turmas.json", "w", encoding="utf-8") as arquivo:
-        arquivo.write(dados)
-
-# Remove uma turma com base no seu id.
-# Parâmetro: o id da turma a ser removida.
-def remover_turma(id):
-    turmas = obter_todas_turmas()
-    if id in turmas.keys():
-        turmas.pop(id)
-        salvar_turmas(turmas)
-
-# Parâmetros: nome, professor e data de início (campos da nova turma)
-# A função tem a finalidade de adicionar uma nova turma ao dicionário de turmas.
-def inserir_turma(nome, professor, data_de_inicio):
-    turmas = obter_todas_turmas()
-    turma = {}
-    turma["nome"] = nome
-    turma["professor"] = professor
-    turma["data_de_inicio"] = data_de_inicio
-    turmas[obter_novo_id()] = turma
-    salvar_turmas(turmas)
-
 #Editando turmas:
 #Essa função tem a finalidade de editar as informações de uma turma existente.
 # Parâmetros: 
 #   id da turma existente, 
 #   nome, professor e data de início (campos a serem modificados)
-def editar_turmas(id, nome, professor, data_de_inicio):
-    turmas = obter_todas_turmas()
+# Retorno:
+#   True se bem sucedido / False caso não exista o id solicitado
+def editar_turma(id, nome, professor, data_de_inicio):
+    turmas = _listar_todas_turmas()
     if id in turmas.keys():
         turma = turmas[id]
         turma["nome"] = nome
         turma["professor"] = professor
         turma["data_de_inicio"] = data_de_inicio
-        salvar_turmas(turmas)
+        _salvar_turmas(turmas)
+        return True
+    else:
+        return False
+    
+# Retorna:
+#    um dicionário onde cada turma é um par chave-valor
+def _listar_todas_turmas():
+    return {
+        "1": {
+            "nome": "turma1",
+            "professor": "Nadalete",
+            "data_de_inicio": "21/02/2023"
+        }
+    }
+
+# Parâmetro: um dicionário onde cada turma é um par chave-valor
+# Retorna:
+#   True se a operação for bem sucedida
+def _salvar_turmas(turmas):
+    dados = json.dumps(turmas, indent=4)
+    with open("dados/turmas.json", "w", encoding="utf-8") as arquivo:
+        arquivo.write(dados)
+    return True
--- testes/gerenciador_turmas.teste.py
+++ testes/gerenciador_turmas.teste.py
@@ -5,60 +5,8 @@ sys.path.append('.')
 import regra_de_negocio.gerenciador_turmas as gt
 
 class TesteGerenciadorTurmas(unittest.TestCase):
-    def teste_obter_todos_grupos(self):
-        esperado =  {
-                        "1": {
-                            "nome": "turma1",
-                            "professor": "Nadalete",
-                            "data_de_inicio":"21/02/2023"
-                        },
-                        "2": {
-                            "nome": "Turma da manhã",
-                            "professor": "Gorete",
-                            "data_de_inicio":"21/02/2023"
-                        }
-                    }
-        turmas = gt.obter_todas_turmas()
-        self.assertEqual(esperado, turmas, "verifica a leitura e desserialização do arquivo turmas.json")
-
-    def teste_obter_ultimo_id(self):
-        semente =  {
-                        "1": {
-                            "nome": "turma1",
-                            "professor": "Nadalete",
-                            "data_de_inicio":"21/02/2023"
-                        },
-                        "2": {
-                            "nome": "Turma da manhã",
-                            "professor": "Gorete",
-                            "data_de_inicio":"21/02/2023"
-                        }
-                    }
-        gt.salvar_turmas(semente)
-        esperado = "2"
-        ultimoid = gt.obter_ultimo_id()
-        self.assertEqual(esperado, ultimoid, "verificao o último id do arquivo turmas.json")
-    
-    def teste_obter_novo_id(self):
-        semente =  {
-                        "1": {
-                            "nome": "turma1",
-                            "professor": "Nadalete",
-                            "data_de_inicio":"21/02/2023"
-                        },
-                        "2": {
-                            "nome": "Turma da manhã",
-                            "professor": "Gorete",
-                            "data_de_inicio":"21/02/2023"
-                        }
-                    }
-        gt.salvar_turmas(semente)
-        esperado = "3"
-        novoid = gt.obter_novo_id()
-        self.assertEqual(esperado, novoid, "verifica a inserção de um novo id no arquivo turmas.json") 
-    
     def teste_salvar_turmas(self):
-        esperado =  {
+        turmas =  {
                         "1": {
                             "nome": "turma1",
                             "professor": "Nadalete",
@@ -70,98 +18,18 @@ class TesteGerenciadorTurmas(unittest.TestCase):
                             "data_de_inicio":"21/02/2023"
                         }
                     }
-        gt.salvar_turmas(esperado)
-        obtido = gt.obter_todas_turmas()
+        obtido = gt._salvar_turmas(turmas)
+        esperado = True
         self.assertEqual(esperado, obtido, "verifica se a inserção ou remoção foram salvos")
 
-    def teste_remover_turma(self):
-        semente =  {
-                        "1": {
-                            "nome": "turma1",
-                            "professor": "Nadalete",
-                            "data_de_inicio":"21/02/2023"
-                        },
-                        "2": {
-                            "nome": "Turma da manhã",
-                            "professor": "Gorete",
-                            "data_de_inicio":"21/02/2023"
-                        }
-                    }
-        gt.salvar_turmas(semente)
-        gt.remover_turma("2")
-        esperado = {
-                        "1": {
-                            "nome": "turma1",
-                            "professor": "Nadalete",
-                            "data_de_inicio":"21/02/2023"
-                        }
-                    }
-        obtido = gt.obter_todas_turmas()
-        self.assertEqual(esperado, obtido, "verifica se a remoção ocorreu")
-    
-    def teste_inserir_turma(self):
-        semente =  {
-                        "1": {
-                            "nome": "turma1",
-                            "professor": "Nadalete",
-                            "data_de_inicio":"21/02/2023"
-                        },
-                        "2": {
-                            "nome": "Turma da manhã",
-                            "professor": "Gorete",
-                            "data_de_inicio":"21/02/2023"
-                        }
-                    }
-        gt.salvar_turmas(semente)
-        gt.inserir_turma("Turma da noite", "Sabha", "21/02/2023")
-        esperado = {
-                        "1": {
-                            "nome": "turma1",
-                            "professor": "Nadalete",
-                            "data_de_inicio":"21/02/2023"
-                        },
-                        "2": {
-                            "nome": "Turma da manhã",
-                            "professor": "Gorete",
-                            "data_de_inicio":"21/02/2023"
-                        },
-                        "3": {
-                            "nome": "Turma da noite",
-                            "professor": "Sabha",
-                            "data_de_inicio":"21/02/2023"
-                        }
-                    }
-        obtido = gt.obter_todas_turmas()
-        self.assertEqual(esperado, obtido, "verifica se a inserção ocorreu")
-    
-    def teste_editar_turmas(self):
-        semente =  {
-                        "1": {
-                            "nome": "turma1",
-                            "professor": "Nadalete",
-                            "data_de_inicio":"21/02/2023"
-                        },
-                        "2": {
-                            "nome": "Turma da manhã",
-                            "professor": "Gorete",
-                            "data_de_inicio":"21/02/2023"
-                        }
-                    }
-        gt.salvar_turmas(semente)
-        esperado = {
-                        "1": {
-                            "nome": "turma7",
-                            "professor": "Nadaleta",
-                            "data_de_inicio":"21/03/2023"
-                        },
-                        "2": {
-                            "nome": "Turma da manhã",
-                            "professor": "Gorete",
-                            "data_de_inicio":"21/02/2023"
-                        }
-                    }
-        gt.editar_turmas("1", "turma7", "Nadaleta", "21/03/2023")
-        obtido = gt.obter_todas_turmas()
+    def teste_editar_turma_existente(self):
+        esperado = True
+        obtido = gt.editar_turma("1", "turma7", "Nadaleta", "21/03/2023")
+        self.assertEqual(esperado, obtido, "verificar se as alterações foram salvas")
+
+    def teste_editar_turma_nao_existente(self):
+        esperado = False
+        obtido = gt.editar_turma("2", "turma7", "Nadaleta", "21/03/2023")
         self.assertEqual(esperado, obtido, "verificar se as alterações foram salvas")
 
 if __name__ == '__main__':
