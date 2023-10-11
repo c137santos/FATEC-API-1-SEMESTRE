0c207dbfe604941c32e69707f6c603544993afe0
marilia-borgo <marilia.bm01@gmail.com>
2023-10-11 17:25:54 -0300

    black e ruff

--- views/funcoes_request_response.py
+++ views/funcoes_request_response.py
@@ -1,6 +1,5 @@
 from wgsi import HttpResponse, JsonResponse
 from regra_de_negocio.service import busca_dados_json
-from regra_de_negocio.global_settings import read_global_settings
 
 
 def hola_mundinho(request):
@@ -19,4 +18,3 @@ def edit_aluno(request, id):
 def get_global_settings():
     global_settings = global_settings.read_global_settings()
     return JsonResponse(global_settings)
-
