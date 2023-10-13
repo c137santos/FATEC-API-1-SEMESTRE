d1d54b4113f5afcf020c928d55b221aa24018f6a
MateusMSoares <mateus.marques100@hotmail.com>
2023-10-13 02:46:53 -0300

    read global settings no funcoes request e response

--- views/funcoes_request_response.py
+++ views/funcoes_request_response.py
@@ -1,5 +1,6 @@
 from wgsi import HttpResponse, JsonResponse
 from regra_de_negocio.service import busca_dados_json, busca_turmas, buscar_grupos, cria_turma
+from regra_de_negocio.global_settings import read_global_settings
 
 
 def hola_mundinho(request):
@@ -14,6 +15,9 @@ def get_arquivos_json(request):
 def edit_aluno(request, id):
     return JsonResponse({"message": f"Editando o aluno com ID {id}."})
 
+def get_global_settings(request):
+    global_settings = read_global_settings()
+    return JsonResponse(global_settings)
 
 def get_turmas(request):
     turmas_data = busca_turmas()
