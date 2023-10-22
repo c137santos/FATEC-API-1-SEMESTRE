0ca6bf008a3539371690d0bf83bd1d8ef8e51487
RuthMira <143049504+RuthMira@users.noreply.github.com>
2023-10-22 15:19:03 -0300

    adicao

---
+++ regra_de_negocio/gerenciador_grupos.py
@@ -0,0 +1,18 @@
+def obter_grupos():
+    grupos = {}
+
+    grupo1 = {}
+    grupo1["turma"] = 1
+    grupo1["nome"] = "team bee"
+
+    grupo2 = {}
+    grupo2["turma"] = 2
+    grupo2["nome"] = "team tatata"
+
+    grupo3 = {}
+    grupo3["turma"] = 3
+    grupo3["nome"] = "team tech"
+
+    grupos["1"] = grupo1
+    grupos["2"] = grupo2
+    grupos["3"] = grupo3
