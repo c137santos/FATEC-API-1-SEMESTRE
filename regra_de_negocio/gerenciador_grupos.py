273c3d67fdf9b9c1748b8f0956a0653bfeeabca3
MateusMSoares <mateus.marques100@hotmail.com>
2023-10-23 18:26:35 -0300

    Arrumando a minha bagunça

--- regra_de_negocio/gerenciador_grupos.py
+++ regra_de_negocio/gerenciador_grupos.py
@@ -1,5 +1,8 @@
 import json
 
+#Função criada para fazer a leitura do arquivo grupos.json localizado na pasta "dados"
+#O módulo json é usado para carregar o conteúdo do arquivo JSON no objeto grupos_Data. O json.load() converte o JSON em um objeto Python
+#Retorno: conteúdo do arquivo como um objeto Python.
 
 def buscando_grupos():
     with open("dados/grupos.json", "r", encoding="utf-8") as f:
