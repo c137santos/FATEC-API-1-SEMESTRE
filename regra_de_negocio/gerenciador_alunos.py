d53622714dac408972a831ec5bc1559898bf488e
marilia-borgo <marilia.bm01@gmail.com>
2023-10-30 11:19:13 -0300

    faz a parte de excluir alunos

--- regra_de_negocio/gerenciador_alunos.py
+++ regra_de_negocio/gerenciador_alunos.py
@@ -5,3 +5,18 @@ def listar_alunos():
     with open("dados/alunos.json", "r", encoding="utf-8") as f:
         alunos = json.load(f)
     return alunos
+
+def apagar_aluno(id):
+    alunos = listar_alunos()
+    if id in alunos.keys():
+        alunos.pop(id)
+        _salvar_alunos(alunos)
+        return True
+    else:
+        return False
+
+def _salvar_alunos(alunos):
+    dados = json.dumps(alunos, indent=4)
+    with open("dados/alunos.json", "w", encoding="utf-8") as arquivo:
+        arquivo.write(dados)
+        return True
