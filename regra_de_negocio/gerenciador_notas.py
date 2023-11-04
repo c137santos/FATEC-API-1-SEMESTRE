f3aa615a5b0015dfd66e9347b8f0c1e55058d700
Sara Nahra <sara.nahra@fatec.sp.gov.br>
2023-11-03 21:34:06 -0300

    mudando para 2 casas decimais a média do FEE

--- regra_de_negocio/gerenciador_notas.py
+++ regra_de_negocio/gerenciador_notas.py
@@ -24,8 +24,8 @@ def _calcular_fee_turma_aluno(id_turma, id_aluno):
         soma_das_notas += peso_nota * valor
         soma_dos_pesos += peso_nota
     if len(notas_por_turma_aluno) > 0:
-        resultado = soma_das_notas / float(soma_dos_pesos)
-        return round (resultado, 2)
+        media_fee = soma_das_notas / float(soma_dos_pesos)
+        return round (media_fee, 2)
     else:
         return 0.0
 
