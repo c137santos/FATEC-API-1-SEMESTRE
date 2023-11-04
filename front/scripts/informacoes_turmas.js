7061dc525bf31ea97c0a8a3c76ba79aeb9a4d153
MateusMSoares <mateus.marques100@hotmail.com>
2023-11-04 04:07:40 -0300

    realizado correcoes

--- front/scripts/informacoes_turmas.js
+++ front/scripts/informacoes_turmas.js
@@ -8,8 +8,9 @@ async function preencher_info_turma(id) {
     const nomeTurmaElement = document.querySelector(".fnomeTurma");
     const nomeProfessorElement = document.querySelector(".fnomeProfessor");
     const cicloPeso = document.querySelector(".ciclo-peso");
-    const quantidadeAlunosElement = document.getElementById('fquantidadeAlunos');
-    const dataInicioElement = document.getElementById('fdataInicio');
+    const quantidadeAlunosElement =
+      document.getElementById("fquantidadeAlunos");
+    const dataInicioElement = document.getElementById("fdataInicio");
     nomeTurmaElement.textContent = turma["nome"];
     nomeProfessorElement.textContent = turma["professor"];
     quantidadeAlunosElement.innerText = alunos ? Object.keys(alunos).length : 0;
@@ -18,17 +19,17 @@ async function preencher_info_turma(id) {
       const pesoData = PesoCiclo[chave];
       const NomeCiclo = document.createElement("label");
       NomeCiclo.className = "ciclo";
-      NomeCiclo.htmlFor = `pesoCiclo=${chave}`
+      NomeCiclo.htmlFor = `pesoCiclo=${chave}`;
       NomeCiclo.innerHTML = `C${pesoData.numero_ciclo}:`;
 
       const PesoNota = document.createElement("input");
       PesoNota.className = "peso";
-      PesoNota.id = `pesoCiclo=${chave}`
+      PesoNota.id = `pesoCiclo=${chave}`;
       PesoNota.value = pesoData.peso_nota;
 
-      PesoNota.addEventListener("keypress", (e) => { 
-        if(e.key === 'Enter'){
-          requisitar_salvar_ciclo_peso() 
+      PesoNota.addEventListener("keypress", (e) => {
+        if (e.key === "Enter") {
+          requisitar_salvar_ciclo_peso();
         }
       });
 
@@ -91,7 +92,6 @@ function criarCampoNota(alunoId, notasAlunos) {
       const id_ciclo = notaAluno.id_ciclo;
       const cicloAberto = notaAluno.edicao_habilitada;
       const valorNota = notaAluno.valor;
-      
 
       if (id_aluno == alunoId) {
         const InputNotas = document.createElement("input");
@@ -125,9 +125,9 @@ function adicionarMediaAoAluno(alunoId, notasAlunos, PesoCiclo, id_turma) {
   const mediaAluno = document.createElement("div");
   mediaAluno.className = "media";
   mediaAluno.id = `mediaAlunoId=${alunoId}`;
-  Promise
-    .resolve(obter_fee_turma_aluno(id_turma, alunoId))
-    .then((fee) => mediaAluno.textContent = fee)
+  Promise.resolve(obter_fee_turma_aluno(id_turma, alunoId)).then(
+    (fee) => (mediaAluno.textContent = fee)
+  );
   alunoSquare.appendChild(mediaAluno);
 }
 
@@ -141,43 +141,53 @@ function requisitar_editar_nota(alunos) {
   const requestBody = {};
 
   let notasInvalidas = false; // Variável para verificar se há notas inválidas
+  let NenhumaNotaModificada = true;
 
-  notasEditaveis.forEach((nota) => {
-    const id_nota = nota.id.split("id_nota=")[1].split(",")[0];
-    const id_turma = nota.id.split("id_turma=")[1].split(",")[0];
-    const id_aluno = nota.id.split("id_aluno=")[1].split(",")[0];
-    const id_ciclo = nota.id.split("id_ciclo=")[1];
-    const valor = parseFloat(nota.value); // Converter o valor para número
+  if (notasEditaveis.length > 0) {
+    notasEditaveis.forEach((nota) => {
+      // caso a nota nao for alterada
 
-    if (isNaN(valor) || valor < 0 || valor > 10) {
-      // Se a nota não for um número ou estiver fora do intervalo, marca como inválida
-      notasInvalidas = true;
-      nota.classList.add("nota-invalida"); // Adiciona uma classe para destacar a nota inválida visualmente
-    } else {
-      // Se a nota for válida, adiciona ao requestBody
-      requestBody[id_nota] = {
-        id_turma: id_turma,
-        id_aluno: id_aluno,
-        id_ciclo: id_ciclo,
-        valor: valor,
-      };
-    }
-  });
+      const id_nota = nota.id.split("id_nota=")[1].split(",")[0];
+      const id_turma = nota.id.split("id_turma=")[1].split(",")[0];
+      const id_aluno = nota.id.split("id_aluno=")[1].split(",")[0];
+      const id_ciclo = nota.id.split("id_ciclo=")[1];
+      const valor = parseFloat(nota.value); // Converter o valor para número
+      const valorOriginal = parseFloat(nota.dataset.ValorOriginal); // Converte o valor original para número
 
-  if (notasInvalidas) {
-    // Exibe uma mensagem de erro ao usuário
-    alert("Todas as notas devem estar entre 0 e 10.");
-    return;
-  }
-
-  const decisao_usuario = criar_modal_confirmar_edicao(requestBody, alunos);
-  if (decisao_usuario) {
-    fetch(`http://localhost:8080/api/v1/notas/editar`, {
-      method: "POST",
-      body: JSON.stringify(requestBody),
+      if (isNaN(valor) || valor < 0 || valor > 10) {
+        // Se a nota não for um número ou estiver fora do intervalo, marca como inválida
+        notasInvalidas = true;
+        nota.classList.add("nota-invalida"); // Adiciona uma classe para destacar a nota inválida visualmente
+      }
+      // Verificar se o valor é estritamente diferente, para só sobreescrever uma nota diferente
+      if (valor !== valorOriginal) {
+        NenhumaNotaModificada = false;
+        requestBody[id_nota] = {
+          id_turma: id_turma,
+          id_aluno: id_aluno,
+          id_ciclo: id_ciclo,
+          valor: valor,
+        };
+      }
     });
-  } else {
-    // O usuário optou por não prosseguir, você pode adicionar alguma lógica aqui se necessário
+
+    if (NenhumaNotaModificada) {
+      return;
+    }
+    if (notasInvalidas) {
+      // Exibe uma mensagem de erro ao usuário
+      alert("Todas as notas devem estar entre 0 e 10.");
+      return;
+    }
+    const decisao_usuario = criar_modal_confirmar_edicao(requestBody, alunos);
+    if (decisao_usuario) {
+      fetch(`http://localhost:8080/api/v1/notas/editar`, {
+        method: "POST",
+        body: JSON.stringify(requestBody),
+      });
+    } else {
+      // O usuário optou por não prosseguir, você pode adicionar alguma lógica aqui se necessário
+    }
   }
 }
 
@@ -244,27 +254,37 @@ function obter_id() {
   //  * @returns {string} id - o identificador único
 }
 
-async function requisitar_salvar_ciclo_peso(){
-  if(!confirm("Essa operação afetará o FEE dos alunos.\nDeseja prosseguir mesmo assim?")){
-    return
+async function requisitar_salvar_ciclo_peso() {
+  if (
+    !confirm(
+      "Essa operação afetará o FEE dos alunos.\nDeseja prosseguir mesmo assim?"
+    )
+  ) {
+    return;
   }
-  console.log("Editando os ciclos...")
-  const turma_id = obter_id()
-  const ciclos = await listar_ciclos_turma(turma_id)
-  for(var id_ciclo in ciclos){
-    console.log(`Salvando o ciclo ${id_ciclo}`)
-    elementoCiclo = document.getElementById(`pesoCiclo=${id_ciclo}`)
-    ciclo = ciclos[id_ciclo]
-    ciclo["peso_nota"] = elementoCiclo.value
-    console.log(ciclo)
-    res = await fetch(`http://localhost:8080/api/v1/ciclos/editar/${id_ciclo}`,{method:"POST", body:JSON.stringify(ciclo)})
-    console.log(res)
+  console.log("Editando os ciclos...");
+  const turma_id = obter_id();
+  const ciclos = await listar_ciclos_turma(turma_id);
+  for (var id_ciclo in ciclos) {
+    console.log(`Salvando o ciclo ${id_ciclo}`);
+    elementoCiclo = document.getElementById(`pesoCiclo=${id_ciclo}`);
+    ciclo = ciclos[id_ciclo];
+    ciclo["peso_nota"] = elementoCiclo.value;
+    console.log(ciclo);
+    res = await fetch(
+      `http://localhost:8080/api/v1/ciclos/editar/${id_ciclo}`,
+      { method: "POST", body: JSON.stringify(ciclo) }
+    );
+    console.log(res);
   }
-  location.reload()
+  location.reload();
 }
 
-async function obter_fee_turma_aluno(id_turma, id_aluno){
-  const response = await fetch (`http://localhost:8080/api/v1/notas/fee/obter/${id_turma}/${id_aluno}`, {method: "GET"})
-  const fee = await response.json()
-  return fee ? fee.valor : 0.0
+async function obter_fee_turma_aluno(id_turma, id_aluno) {
+  const response = await fetch(
+    `http://localhost:8080/api/v1/notas/fee/obter/${id_turma}/${id_aluno}`,
+    { method: "GET" }
+  );
+  const fee = await response.json();
+  return fee ? fee.valor : 0.0;
 }
