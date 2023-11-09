bb853e040428de4c6639234fb55afce28b9143ea
marilia-borgo <marilia.bm01@gmail.com>
2023-11-09 17:15:33 -0300

    faz o ciclo dinâmico

--- regra_de_negocio/service.py
+++ regra_de_negocio/service.py
@@ -1,11 +1,23 @@
-import regra_de_negocio.gerenciador_turmas as gt
+import regra_de_negocio.gerenciador_turmas as gerenciador_turmas
+import regra_de_negocio.global_settings as global_settings
+import regra_de_negocio.gerenciador_ciclos as gerenciador_ciclos
+import regra_de_negocio.gerenciador_turmas_alunos as gerenciador_turmas_alunos
+import regra_de_negocio.gerenciador_notas as gerenciador_notas
 
 
 def busca_turmas():  # retorna todas turmas
-    turmas_data = gt.busca_turmas()
+    turmas_data = gerenciador_turmas.busca_turmas()
     return turmas_data
 
 
 def cria_turma(dados_nova_turma):
-    resposta = gt.criacao_turma(dados_nova_turma)
+    resposta = gerenciador_turmas.criacao_turma(dados_nova_turma)
+    info_global_settings= global_settings.read_global_settings()
+    id_nova_turma_str = str(resposta["id_nova_turma"])
+    #cria ciclos para a turma
+    gerenciador_ciclos.cria_ciclos_pra_turma(id_nova_turma_str, info_global_settings)
+    #cria notas para o aluno
+    ciclos = gerenciador_ciclos.listar_ciclos_por_id_turma(id_nova_turma_str)
+    alunos = gerenciador_turmas_alunos.listar_alunos_turma(id_nova_turma_str)
+    gerenciador_notas.adicionar_notas_aluno_turma(ciclos, alunos, id_nova_turma_str)
     return resposta
