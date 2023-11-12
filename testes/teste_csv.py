d3029cbb76d2ce749d2422724667f379c15b7876
MateusMSoares <mateus.marques100@hotmail.com>
2023-11-12 09:37:06 -0300

    ajustes

--- testes/teste_csv.py
+++ testes/teste_csv.py
@@ -1,5 +1,6 @@
 import csv
 import json
+import re
 
 def listar_alunos():
     with open("testes/alunos.json", "r", encoding="utf-8") as f:
@@ -14,7 +15,46 @@ def listar_turma_aluno():
 def ler_csv():
     with open("testes/novos_alunos.csv", newline='', encoding='utf-8-sig') as arquivo_csv:
         leitor_csv = list(csv.DictReader(arquivo_csv, delimiter=';'))
+        verifica_csv(leitor_csv)
         return leitor_csv
+    
+def valida_nome(nome):
+    # Verifica se o nome contém apenas letras e espaços
+    return bool(re.match(r'^[A-Za-z\s]+$', nome))
+
+def valida_genero(genero):
+    # Verifica se o gênero contém apenas letras
+    return bool(re.match(r'^[A-Za-z]+$', genero))
+
+def valida_data(data):
+    # Verifica se a data está no formato dd/mm/aaaa
+    return bool(re.match(r'^\d{2}/\d{2}/\d{4}$', data))
+
+def verifica_csv(leitor_csv):
+    # Verifica se a primeira linha contém os cabeçalhos esperados
+    cabecalhos_esperados = ["Nome completo do aluno", "Genêro", "Data de Nascimento"]
+    if leitor_csv.fieldnames != cabecalhos_esperados:
+        raise ValueError("Cabeçalhos do CSV não correspondem aos esperados.")
+
+    # Verifica cada linha do CSV
+    for aluno in leitor_csv:
+        # Verifica se todos os campos estão presentes e não são vazios
+        if not aluno["Nome completo do aluno"] or not aluno["Genêro"] or not aluno["Data de Nascimento"]:
+            raise ValueError("Campos incompletos no CSV.")
+
+        # Verifica se o nome e o gênero contêm apenas letras
+        if not valida_nome(aluno["Nome completo do aluno"]):
+            raise ValueError(f"Nome inválido em: {aluno['Nome completo do aluno']}")
+
+        if not valida_genero(aluno["Genêro"]):
+            raise ValueError(f"Gênero inválido em: {aluno['Genêro']}")
+
+        # Verifica se a data está no formato correto
+        if not valida_data(aluno["Data de Nascimento"]):
+            raise ValueError(f"Data inválida em: {aluno['Data de Nascimento']}")
+
+    print("CSV verificado com sucesso!")
+
 
 def _obter_novo_id(entidade):
     ids_numericos = [0]
