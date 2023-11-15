c5067b4b14cc47db5f4b8c0d9bfcd475d33e26d5
MateusMSoares <mateus.marques100@hotmail.com>
2023-11-14 21:44:20 -0300

    retirado funcao de teste de importacao alunos

--- regra_de_negocio/gerenciador_importacao_alunos.py
+++ regra_de_negocio/gerenciador_importacao_alunos.py
@@ -1,21 +1,6 @@
 import json
 import re
 
-def teste(requisicao, alunos_importados):
-    turma_id = requisicao.get("turma_id")
-    nome_turma = requisicao.get("nome_Turma")
-
-    print(alunos_importados)
-
-    # Iterar sobre os alunos e acessar os valores
-    for aluno in alunos_importados:
-        nome_completo = aluno['Nome completo do aluno']
-        genero = aluno['Genêro']
-        data_nascimento = aluno['Data de Nascimento']
-
-        # Faça o que quiser com os valores, por exemplo, imprimir
-        print(f"turma ID: {turma_id}, nome_turma: {nome_turma}, Nome: {nome_completo}, Gênero: {genero}, Data de Nascimento: {data_nascimento}")
-
 def valida_nome(nome):
     if not re.match(r'^[A-Za-zÀ-ÖØ-öø-ÿ\s]+$', nome):
         raise ValueError(f"Apenas letras no nome: {nome.title()}")
