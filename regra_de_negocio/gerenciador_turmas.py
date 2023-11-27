a00fc1f02ca767901dedc0494131bde9368333e3
MateusMSoares <mateus.marques100@hotmail.com>
2023-11-26 21:39:10 -0300

    retira -1 da data limite para importar alunos

--- regra_de_negocio/gerenciador_turmas.py
+++ regra_de_negocio/gerenciador_turmas.py
@@ -120,9 +120,7 @@ def turmas_nao_iniciadas():
     turmas_nao_iniciadas = {}
     for id_turma in turmas.keys():
         if (
-            datetime.strptime(turmas[id_turma]["data_de_inicio"], "%d/%m/%Y")
-            - timedelta(1)
-            > data_atual
+            datetime.strptime(turmas[id_turma]["data_de_inicio"], "%d/%m/%Y") > data_atual
         ):
             turmas_nao_iniciadas[id_turma] = {"nome": turmas[id_turma]["nome"]}
     return turmas_nao_iniciadas
